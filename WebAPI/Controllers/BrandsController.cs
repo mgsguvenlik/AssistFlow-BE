783ee98b6aa75e66c3eab4a1ce79d01bfdb6cf69
Mehmet Zeki KARA <karamehmetzeki506@gmail.com>
2025-09-29 21:59:38 +0300

    brand controller geliştirmesi

---
+++ WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,18 @@
+﻿using Business.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Dtos.Brand;
+using Model.Dtos.Model;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class BrandsController : CrudControllerBase<BrandCreateDto, BrandUpdateDto, BrandGetDto, long>
+    {
+        public BrandsController(
+            ICrudService<BrandCreateDto, BrandUpdateDto, BrandGetDto, long> service,
+            ILogger<BrandsController> logger) : base(service, logger) { }
+    }
+}
