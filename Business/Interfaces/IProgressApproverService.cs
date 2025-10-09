dbb02ac8183d7eebf34f273100dd3bc191f9fbb6
Mehmet Zeki KARA <karamehmetzeki506@gmail.com>
2025-10-09 21:33:53 +0300

    getby-customerid  metodu

--- Business/Interfaces/IProgressApproverService.cs
+++ Business/Interfaces/IProgressApproverService.cs
@@ -1,6 +1,10 @@
-﻿namespace Business.Interfaces
+﻿using Core.Common;
+using Model.Dtos.ProgressApprover;
+
+namespace Business.Interfaces
 {
     public interface IProgressApproverService
     {
+         Task<ResponseModel<List<ProgressApproverGetDto>>> GetByCustomerIdAsync(long customerId, CancellationToken cancellationToken);
     }
 }
--- WebAPI/Controllers/ProgressApproversController.cs
+++ WebAPI/Controllers/ProgressApproversController.cs
@@ -7,12 +7,23 @@ namespace WebAPI.Controllers
     [ApiController]
     [Route("api/[controller]")]
     [Produces("application/json")]
-    public class ProgressApproversController
-        : CrudControllerBase<ProgressApproverCreateDto, ProgressApproverUpdateDto, ProgressApproverGetDto, long>
+    public class ProgressApproversController : CrudControllerBase<ProgressApproverCreateDto, ProgressApproverUpdateDto, ProgressApproverGetDto, long>
     {
-        public ProgressApproversController(
-            ICrudService<ProgressApproverCreateDto, ProgressApproverUpdateDto, ProgressApproverGetDto, long> service,
-            ILogger<ProgressApproversController> logger)
-            : base(service, logger) { }
+
+        private IProgressApproverService _progressApproverService;
+        public ProgressApproversController(ICrudService<ProgressApproverCreateDto,
+            ProgressApproverUpdateDto, ProgressApproverGetDto, long> service, ILogger<ProgressApproversController> logger, IProgressApproverService progressApproverService) : base(service, logger)
+        {
+            _progressApproverService = progressApproverService;
+        }
+
+        [HttpGet("getby-customerid")]
+        public  async Task<IActionResult> GetByCustomerId(long customerId)
+        {
+            var resp = await _progressApproverService.GetByCustomerIdAsync(customerId, CancellationToken.None);
+            return ToActionResult(resp);
+
+        }
     }
+
 }
