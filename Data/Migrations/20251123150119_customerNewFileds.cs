293425cb213b0e16730e987d81c2f2d729d7063b
Mehmet Zeki KARA <karamehmetzeki506@gmail.com>
2025-11-23 18:09:50 +0300

    file upload and migration

---
+++ Data/Migrations/20251123150119_customerNewFileds.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class customerNewFileds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CashCenter",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LockType",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CashCenter",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "LockType",
+                table: "Customers");
+        }
+    }
+}
