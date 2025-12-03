ff620ab758839adf846465fb6488d9a890d61abf
Furkan TOPAL <furkant_35@hotmail.com>
2025-12-03 18:05:42 +0300

    .

--- Model/Dtos/WorkFlowDtos/YkbDtos/YkbFinalApproval/YkbFinalApprovalGetDto.cs
+++ Model/Dtos/WorkFlowDtos/YkbDtos/YkbFinalApproval/YkbFinalApprovalGetDto.cs
@@ -1,7 +1,9 @@
 ﻿using Core.Enums;
 using Model.Dtos.Customer;
+using Model.Dtos.WorkFlowDtos.TechnicalServiceImage;
 using Model.Dtos.WorkFlowDtos.YkbDtos.YkbReviewLog;
 using Model.Dtos.WorkFlowDtos.YkbDtos.YkbServicesRequestProduct;
+using Model.Dtos.WorkFlowDtos.YkbDtos.YkbTechnicalServiceImage;
 
 namespace Model.Dtos.WorkFlowDtos.YkbDtos.YkbFinalApproval
 {
@@ -16,6 +18,10 @@ namespace Model.Dtos.WorkFlowDtos.YkbDtos.YkbFinalApproval
         public List<YkbWorkFlowReviewLogDto> ReviewLogs { get; set; } = new();
         public List<YkbServicesRequestProductGetDto> Products { get; set; } = new();
         public CustomerGetDto? Customer { get; set; }
+
+        // Resim listeleri
+        public List<YkbTechnicalServiceImageGetDto> ServicesImages { get; set; } = new();
+        public List<TechnicalServiceFormImageGetDto> ServiceRequestFormImages { get; set; } = new();
     }
 
 }
