96e51df574f2e02dc9967edd8a416ccad9133de4
Mehmet Zeki Kara <karamehmetzeki506@gmail.com>
2026-06-12 21:10:18 +0300

    Feedback 27 Kullanıcı filtresi çalışmıyor

---
+++ Core/Common/QueryParams.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Common
+{
+    public class QueryParams
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+        public string? Search { get; set; }
+        public string? Sort { get; set; }
+        public bool Desc { get; set; } = false;
+    }
+
+    public record PagedResult<T>(
+        IReadOnlyList<T> Items,
+        int TotalCount,
+        int Page,
+        int PageSize
+    );
+}
---
+++ Model/Dtos/User/UserQueryParams.cs
@@ -0,0 +1,17 @@
+﻿using Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dtos.User
+{
+    public class UserQueryParams : QueryParams
+    {
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public long? RoleId { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
