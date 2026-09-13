3959eb54755a8bd7eefe269486a7f09de8b23185
Mehmet Zeki KARA <karamehmetzeki506@gmail.com>
2026-09-13 18:24:52 +0300

    tahsilat FAZ 4

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -40,6 +40,7 @@ namespace Business.DependencyResolvers.Autofac
         public void Load(IServiceCollection services)
         {
             services.AddScoped<ICollectionContractReadService, CollectionContractReadService>();
+            services.AddScoped<ICollectionDefinitionReadService, CollectionDefinitionReadService>();
             services.AddOptions<CollectionReadOptions>().BindConfiguration(CollectionReadOptions.SectionName);
             services.AddScoped(typeof(IAuthService), typeof(AuthService));
             services.AddScoped(typeof(IBrandService), typeof(BrandService));
